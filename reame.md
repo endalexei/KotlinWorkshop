ce4e3e62138900a9752d7bd9ec5879adafb17ad6
dalexei <dumitru.alexei@endava.com>
2024-04-03 10:04:33 +0300

    Implement kotest

---
+++ reame.md
@@ -0,0 +1,7 @@
+# **Befor you run** 
+Instal Kotest plugin:
+
+1. Go to Setings > Plugins
+2. Search for Kotest
+3. Install the plugin
+4. Restart IDE
--- src/test/kotlin/NullSafetyTest.kt
+++ src/test/kotlin/NullSafetyTest.kt
@@ -1,31 +1,47 @@
-import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
 
-class NullSafetyTest {
+class NullSafetyTest : ShouldSpec({
 
-    private fun getLength(value: String?): Int? = value?.length
-
-
-    @Test
-    fun getStringLengthWIthNullValue() {
+    /**
+     * Create an inline function that will return either string length or null
+     */
+    should("Get String Length WIth Null Value") {
         val value1 = null
         val value2 = "a valid string"
         println(getLength(value1))
-        assertEquals(getLength(value1), null)
+        getLength(value1) shouldBe null
         println(getLength(value2))
-        assertEquals(getLength(value2), 14)
-
+        getLength(value2) shouldBe 14
     }
 
-
-    @Test
-    fun getEmployAge() {
+    /**
+     * Create functions in class to for validation to pass
+     */
+    should("Get Employ name and age") {
         val company = Company(Endava(Employ()))
-        println("Employ name is ${company.company.employ?.name}")
-        println("Employ age is ${company.company.employ?.age}")
+        company.getEmployName() shouldBe "Sergiu"
+        company.getEmployAge() shouldBe null
     }
-}
+})
+
+fun getLength(value: String?): Int? = value?.length
 
 data class Employ(val name: String = "Sergiu", var age: Int? = null)
 data class Endava(val employ: Employ?)
-data class Company(val company: Endava)
+data class Company(val company: Endava) {
+
+    fun getEmployName(): String? {
+        val company = Company(Endava(Employ()))
+        val name = company.company.employ?.name
+        println("Employ name is $name")
+        return name
+    }
+
+    fun getEmployAge(): Int? {
+        val company = Company(Endava(Employ()))
+        val age = company.company.employ?.age
+        println("Employ age is $age")
+        return age
+    }
+}
