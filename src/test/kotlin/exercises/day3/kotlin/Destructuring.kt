6da249a41cc91bab40bc6bb696d47e937e5bb433
dalexei <dumitru.alexei@endava.com>
2024-04-12 15:51:58 +0300

    Day 3

---
+++ src/test/kotlin/exercises/day3/kotlin/Destructuring.kt
@@ -0,0 +1,31 @@
+package exercises.day3.kotlin
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+
+class Destructuring : ShouldSpec({
+
+    var name = ""
+    var age = 0
+
+    fun destructuring(personValue: Person) {
+        TODO()
+    }
+
+    /**
+     * I would like you to use destructuring for this test
+     * Please USE DESTRUCTURING I know there is other ways
+     */
+    should("Destructure Person objet") {
+        val jan = Person("Jean-Claude Van Damme", 63)
+        destructuring(jan)
+        name shouldBe "Jean-Claude Van Damme"
+        age shouldBe 63
+    }
+
+})
+
+class Person(val personName: String, val age: Int) {
+    //TODO()
+
+}
---
+++ src/test/kotlin/exercises/day3/kotlin/Interfaces.kt
@@ -0,0 +1,18 @@
+package exercises.day3.kotlin
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+
+//val check : isString = TODO()
+
+/**
+ * Please create an typealias and then make test to pass
+ */
+class Interfaces: ShouldSpec( {
+//    val result = check("Wanea") shouldBe true
+//    println(result)
+//    val result2 = check(53) shouldBe false
+//    println(result2)
+})
+
+//typealias isString = TODO()
